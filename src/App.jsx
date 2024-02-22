bff756c06e110a9ec50ba5965d247ca13a7bd407
Ajay Joshi <joshiajay311@gmail.com>
2024-02-22 20:13:09 +0530

    routes added

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,25 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { Dashboard } from "./components/Dashboard"
+import {Landing } from "./components/Landing"
+import "./main.css"
+
+
 function App() {
 
   return (
-    <>
-      <div>
-      <p>Context API</p>
-      <p>Prop Drilling</p>
-      <p>Routing</p>
+    <div>
+      <div style={{display: "flex",backgroundColor: "black", color: "white"}}>
+          <p className="item">This is the topbar</p>
+          <button className="item">Go to dashboard</button>
+          <button className="item">Go to landing Page</button>
       </div>
-    </>
+      <BrowserRouter>
+        <Routes>
+           <Route path="/dashboard" element={<Dashboard />}></Route>
+           <Route path="/" element={<Landing />}></Route>
+        </Routes>
+      </BrowserRouter>
+      </div>
   )
 }
 
